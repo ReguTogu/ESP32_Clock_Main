0d631aa788984bb8513dd3b91d3e65a22563cfb5
ReguTogu <168965102+ReguTogu@users.noreply.github.com>
2024-06-01 17:15:16 +0700

    Update Buzz.h

--- main/Buzz.h
+++ main/Buzz.h
@@ -14,7 +14,7 @@
 #define BUZZER_CHANNEL            LEDC_CHANNEL_0
 #define BUZZER_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
 #define BUZZER_DUTY_ON            (4096) // Set duty to 50%. (2 ** 13) * 50% = 4096
-#define BUZZER_DUTY_OFF           (0) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define BUZZER_DUTY_OFF           (0) // Set duty to 0%.
 #define BUZZER_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
 
 void buzzer_init(void);
